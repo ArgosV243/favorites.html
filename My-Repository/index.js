2b0fb81a86d19b971810427f477f14c585293886
ArgosV243 <130006047+ArgosV243@users.noreply.github.com>
2023-05-16 09:22:13 -0700

    Add files via upload

---
+++ My-Repository/index.js
@@ -0,0 +1,5 @@
+let origionalFunction = () => {
+    document.getElementById("demo").style.color = "blue";
+}
+// object.addEventListener("click", origionalFunction);
+// 'Working very hard to get you what you need'
